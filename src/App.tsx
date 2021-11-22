c1625d58ff09cf0eb5b49c33af1a6730db023524
Leonard <hjeong@smallticket.com>
2021-11-22 10:23:33 +0900

    install dependencies & setting styles and themes
    1. install antd
    2. install styled-components
    3. install styled-normalize
    4. set global styles
    5. set themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      hello
+      <a href="https://www.naver.com">naver</a>
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyle from './styles/global-styled';
+import theme from './styles/theme';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
   document.getElementById('root'),
 );
